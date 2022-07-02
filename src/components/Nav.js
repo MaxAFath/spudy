a8bbb27147a59f313438ef3119188e916af50d89
Max Fathauer <maxfathauer@gmail.com>
2022-07-02 11:16:18 -0400

    fixed nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,17 @@ const [linkedin, setLinkedin] = useState('"www.linkedin.com/in/max-fathauer-0525
 const [resume, setResume] = useState('"https://www.dropbox.com/s/4z5avncldby5gg4/Max%20A%20Resume.pdf?dl=0"');
 
 function Nav() {
-    <section>
-        <div className="linkBox">
-            <ul className="links">
-                <li href={gitrepo}>Github Repos</li>
-                <li href={linkedin}>Linkedin</li>
-                <li href={resume}>Resume</li>
-            </ul>
-        </div>
-    </section>
+    return (
+        <section>
+            <div className="linkBox">
+                <ul className="links">
+                    <li href={gitrepo}>Github Repos</li>
+                    <li href={linkedin}>Linkedin</li>
+                    <li href={resume}>Resume</li>
+                </ul>
+            </div>
+        </section>
+    );
 };
 
 export default Nav;
