9e4e4a84ffd2d83c2df4028326f236834b2d4778
Max Fathauer <maxfathauer@gmail.com>
2022-07-02 11:11:23 -0400

    added to about and nav components

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+    return (
+        <section className="my-5">
+            <div className="hero">
+                <h1 id="about">Max A Fathauer</h1>
+                <p id="heroInfo">
+                    Graduated from Cuyahoga Community College in 2017, with a Associates of Arts.
+                    I am currently studing full-stack web developer at Case Western Reserve University Coding bootcamp.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
