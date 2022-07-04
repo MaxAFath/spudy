f3237549623bb411ed4714ae63964cf4877c0df0
Max Fathauer <maxfathauer@gmail.com>
2022-07-03 20:16:59 -0400

    added to nav section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 
 function Nav() {
+    const [about, setAbout] = useState('#about');
+    const [contact, setContact] = useState('#contact');
+    const [projects, setProjects]= useState('#projects'); 
+
     return (
         <section>
+            <un>
+                <li className="nav"><a href={about}> about</a></li>
+                <li className="nav"><a href={contact}>Contact</a></li>
+                <li className="nav"><a href={projects}>Projects</a></li>
+            </un>
             
         </section>
     );
