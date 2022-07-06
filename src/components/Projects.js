450c786cac29a128c62f7f148b13741571e37a79
Max Fathauer <maxfathauer@gmail.com>
2022-07-06 12:01:11 -0400

    added to projects list, to have the required four cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,25 @@ function Card(){
                 <a href="https://maxafath.github.io/the-zen-project/">
                     <img src="" alt="The Zen Porject"></img>
                 </a>
+                <h4 className="title">Zen Project</h4>
             </div>
             <div className="card">
                 <a href="https://github.com/MaxAFath/project-two">
-                    <img src="" alt="Upcycle"></img>
+                    <img src="" alt="Upcycle" />
                 </a>
+                <h4 className="title">Upcycle</h4>
+            </div>
+            <div className="card">
+                <a href="">
+                    <img src="" alt="Nerd game" />
+                </a>
+                <h4 className="title">The nerd game</h4>
+            </div>
+            <div className="card">
+                <a href="">
+                    <img src="" alt="General norms quiz" />
+                </a>
+                <h4 className="title">General Norms quiz</h4>
             </div>
         </section>
     );
